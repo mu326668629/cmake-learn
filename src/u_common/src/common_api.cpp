8092ac48adc89b66059919bc909cfa19b58080d2
yanglc <yanglc@163.com>
2021-09-18 23:10:58 +0800

    init

---
+++ src/u_common/src/common_api.cpp
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) default
+ * DATE: 2021-09-18
+ */
+
+#include "common_api.h"
+
+namespace common {
+
+CCommonApi::CCommonApi() {
+}
+
+CCommonApi::~CCommonApi() {
+}
+
+}  // namespace common
---
+++ src/u_common/src/common_api.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) default
+ * DATE: 2021-09-18
+ */
+
+#ifndef __COMMON_API_H__
+#define __COMMON_API_H__
+
+namespace common {
+
+class CCommonApi {
+ public:
+  CCommonApi();
+  ~CCommonApi();
+
+ private:
+};
+
+}  // namespace common
+#endif
